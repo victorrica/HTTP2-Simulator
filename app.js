321c4094944654f8f45a3e505e6f92e3935bc81b
PJH <webdev.pjh@gmail.com>
2015-08-03 20:49:30 +0900

    route change

--- app.js
+++ app.js
@@ -18,23 +18,21 @@ app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.use(express.logger('dev'));
 app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/',function(req,res){
-   res.render('index',{title:"HTTP2-Simulator Main"});
-});
+app.get('/', routes.index);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+  console.log('HTTP2-Simulator server listening on port ' + app.get('port'));
 });
