f0e1f781fa3a29cc9d4ba527a81648a149cd443d
PJH <webdev.pjh@gmail.com>
2015-08-26 10:50:46 +0900

    Add og type Markup

--- routes/index.js
+++ routes/index.js
@@ -4,35 +4,35 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'HTTP2-Simulator::Main' });
+  res.render('index', { title: 'HTTP/2 Simulator::Main' });
 };
 
 
 exports.Webpagetest = function(req, res){
-  res.render('wpt_result', { title: 'HTTP2-Simulator::Webpagetest' });
+  res.render('wpt_result', { title: 'HTTP/2 Simulator::Webpagetest' });
 };
 
 exports.contactus = function(req, res){
-  res.render('contactus', { title: 'HTTP2-Simulator::Contact Us' });
+  res.render('contactus', { title: 'HTTP/2 Simulator::Contact Us' });
 };
 
 exports.rank = function(req, res){
-  res.render('rank', { title: 'HTTP2-Simulator::Rank' });
+  res.render('rank', { title: 'HTTP/2 Simulator::Rank' });
 };
 
 exports.result = function(req, res){
-  res.render('result', { title: 'HTTP2-Simulator::Result' });
+  res.render('result', { title: 'HTTP/2 Simulator::Result' });
 };
 
 exports.check_result = function(req, res){
-  res.render('check_result', { title: 'HTTP2-Simulator::Check Result' });
+  res.render('check_result', { title: 'HTTP/2 Simulator::Check Result' });
 };
 
 
 exports.progress_page = function(req, res){
-  res.render('progress_page', { title: 'HTTP2-Simulator::Progress Page' });
+  res.render('progress_page', { title: 'HTTP/2 Simulator::Progress Page' });
 };
 
 exports.mysql = function(req, res){
-  res.render('mysql', { title: 'HTTP2-Simulator::Mysql test page' });
+  res.render('mysql', { title: 'HTTP/2 Simulator::Mysql test page' });
 };
