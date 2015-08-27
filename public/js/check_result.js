d8863bb261c1084eeaf9692f648e60e9dbefb1e3
jang4427 <01692204427@naver.com>
2015-08-27 09:32:08 +0900

    test

--- public/js/check_result.js
+++ public/js/check_result.js
@@ -94,7 +94,7 @@ function startComparison(aDomain) {
         } else if(index = data.search('download') != -1) {
             var text = data.substring(index+24);
             updateBaseText(text);
-        } else if(data.indexOf('wpt_status') != -1) {
+        } else if(data.indexOf('wptstatus') != -1) {
             alert("eeee");
             var text = data.substring(11);
             updateBaseText(text);
